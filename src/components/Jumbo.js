e7112f92f269c9c56efe7c24d8fb3e3836a070a0
Brian Kilpatrick <thebriankilpatrick@gmail.com>
2020-03-24 10:21:26 -0400

    Added components folder, and jumbo component

---
+++ src/components/Jumbo.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Jumbo() {
+    return (
+        <div className="jumbotron jumbotron-fluid text-center">
+            <div className="container">
+                <h1 className="display-4">Employee Directory</h1>
+                <p className="lead">Search through the table of employees below</p>
+            </div>
+        </div>
+    )
+}
+
+export default Jumbo;
