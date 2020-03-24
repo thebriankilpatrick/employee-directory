4a34190bfa41ae82273b43ed13dc785a7aed34b1
Brian Kilpatrick <thebriankilpatrick@gmail.com>
2020-03-24 10:34:17 -0400

    Added Table component, along with table skeleton

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 // import logo from './logo.svg';
 import Jumbo from "./components/Jumbo";
+import Table from "./components/Table";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Jumbo ></Jumbo>
+      <Table ></Table>
     </div>
   );
 }
---
+++ src/components/Table.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+function Table() {
+    return (
+        <div className="container">
+            <table className="table">
+                <thead>
+                    <tr>
+                        <th scope="col">Name</th>
+                        <th scope="col">Email</th>
+                        <th scope="col">Phone</th>
+                        <th scope="col">Birthday</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                    
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    )
+}
+
+export default Table;
